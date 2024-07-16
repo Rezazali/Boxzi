aed4e27b63e588ab38629f867fb9fd2fffb7b3b9
Reza <rezazalipro@gmail.com>
2024-07-16 15:24:17 +0330

    LongestConsecutiveSequence

---
+++ src/main/java/LongestConsecutiveSequence/LongestConsecutiveSequence.java
@@ -0,0 +1,41 @@
+package LongestConsecutiveSequence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int result = longestConsecutive(nums);
+        System.out.println(result);
+    }
+
+    public static int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
